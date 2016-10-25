f094ba5052f6d626cb173c4e0aa99fb441063f51
zuzanatoldyova <zuzana.toldyova@gmail.com>
2016-10-25 03:54:45 +0000

    Agecalculator.

---
+++ agecalculator.js
@@ -0,0 +1,9 @@
+console.log("fuck");
+
+function ageCalculator(name, yearOfBirth, currentYear){
+  var age = currentYear - yearOfBirth;
+  return name + " is " + age.toString() + " years old.";
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
