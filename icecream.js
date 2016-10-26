c4a7495c5d19c2d5be47e3f1aae4c81a78af129b
zuzanatoldyova <zuzana.toldyova@gmail.com>
2016-10-26 00:19:34 +0000

    Go outside program.

---
+++ icecream.js
@@ -0,0 +1,8 @@
+var iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+console.log(iceCreamFlavours.length);
