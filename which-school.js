72b36845b53b4ab446344901fe1aa113306b54c0
zuzanatoldyova <zuzana.toldyova@gmail.com>
2016-10-26 23:19:12 +0000

    Loops exercises.

---
+++ which-school.js
@@ -0,0 +1,10 @@
+function whichSchool(age){
+  if (age < 13) {
+    return "Elementary School";
+  } else if (age >= 13 && age <= 18){
+    return "Secondary School"
+  } else {
+    return "Lighthouse Labs"
+  }
+
+}
